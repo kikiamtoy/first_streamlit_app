da468f12e26da3617f8d7fbd1eb0222268ea1cf5
kikiamtoy <143485746+kikiamtoy@users.noreply.github.com>
2023-08-31 23:18:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,19 +30,19 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   #streamlit.write('The user entered ', fruit_choice)
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
-else:
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    
   
-
-  #import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  #streamlit.text(fruityvice_response)
-  
-  # write your own comment -what does the next line do? 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # write your own comment - what does this do?
-  streamlit.dataframe(fruityvice_normalized)
+    #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response)
+    
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
